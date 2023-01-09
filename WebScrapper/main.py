e885052d132419083cab300b4e3db537896cf9e4
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2023-01-09 16:01:31 +0530

    Add files via upload

---
+++ WebScrapper/main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_link(url_link):
+    url = url_link #<---- enter the web address you want to fetch links from!
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    anchors = soup.find_all('a')
+
+    all_links = set(anchors)
+
+    with open('output.txt', 'w') as f:
+        f.write('<-----All Links:----->\n')
+
+    for link in all_links:
+        with open('output.txt', 'a') as f:
+            if(link.get('href') != '#'):
+                new_links = ''
+        
+                if(link.get('href').startswith('/')):
+                    new_links = url_link + link.get('href') #<---- enter the web address you want to fetch links from!
+                    # print(new_links)
+
+                if(link.get('href').startswith(url_link)): #<---- enter the web address you want to fetch links from!
+                    new_links = link.get('href')
+                    # print(new_links)
+
+                store_links = f.write(new_links + '\n')
+    
+    return store_links
+
+
+web_url = input("Enter a web address: ")
+
+get_link(web_url)
+
+
