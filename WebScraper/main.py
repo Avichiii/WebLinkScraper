750494a8323f089bb767a2663dee50e943a0d06e
abhijit <abhijitnyk001@gmail.com>
2023-04-15 20:39:08 +0530

    modifying webscraper

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -3,39 +3,27 @@ from bs4 import BeautifulSoup
 
 
 def get_link(url_link):
-    url = url_link #<---- enteres the web address you want to fetch links from!
-
-    r = requests.get(url)
-
-    soup = BeautifulSoup(r.content, 'html.parser')
-
+    r = requests.get(url_link)
+    print(r.status_code)
+    soup = BeautifulSoup(r.content, 'lxml')
     anchors = soup.find_all('a')
 
-    all_links = set(anchors)
+    for link in anchors:
+        if 'href' in link.attrs:
+            if link['href'].startswith('/'):
+                with open('links.txt', 'a') as f:
+                    f.write(url_link + link['href'] + '\n')
+                    f.write('\n')
+            else:
+                with open('links.txt', 'a') as f:
+                    f.write(link['href'] + '\n')
+                    f.write('\n')
 
-    with open('output.txt', 'w') as f:
-        f.write('<-----All Links:----->\n')
-
-    for link in all_links:
-        with open('output.txt', 'a') as f:
-            if(link.get('href') != '#'):
-                new_links = ''
-        
-                if(link.get('href').startswith('/')):
-                    new_links = url_link + link.get('href') #<---- enteres the web address you want to fetch links from!
-                    # print(new_links)
-
-                if(link.get('href').startswith(url_link)): #<---- enteres the web address you want to fetch links from!
-                    new_links = link.get('href')
-                    # print(new_links)
-
-                store_links = f.write(new_links + '\n')
-    
-    return store_links
+if __name__ == '__main__':
+    web_url = input("Enter a web address: ")
+    get_link(f"https://{web_url}")
 
 
-web_url = input("Enter a web address: ")
 
-get_link(web_url)
 
 
