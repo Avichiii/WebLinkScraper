980808f26d27bf99e0db657e531592191e798ea2
abhijit <abhijitnyk001@gmail.com>
2023-04-16 14:34:32 +0530

    adding www in the input

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -24,7 +24,7 @@ def get_link(url_link):
 
 if __name__ == '__main__':
     web_url = input("Enter a web address: ")
-    get_link(f"https://{web_url}")
+    get_link(f"https://www.{web_url}")
 
 
 
