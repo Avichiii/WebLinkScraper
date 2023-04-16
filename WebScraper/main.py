c24bc01edca4fcd2b3f044dde8f8d62de580962d
abhijit <abhijitnyk001@gmail.com>
2023-04-16 14:31:07 +0530

    exception handling

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -3,21 +3,24 @@ from bs4 import BeautifulSoup
 
 
 def get_link(url_link):
-    r = requests.get(url_link)
-    print(r.status_code)
-    soup = BeautifulSoup(r.content, 'lxml')
-    anchors = soup.find_all('a')
+    try:
+        r = requests.get(url_link)
+        print(r.status_code)
+        soup = BeautifulSoup(r.content, 'lxml')
+        anchors = soup.find_all('a')
 
-    for link in anchors:
-        if 'href' in link.attrs:
-            if link['href'].startswith('/'):
-                with open('links.txt', 'a') as f:
-                    f.write(url_link + link['href'] + '\n')
-                    f.write('\n')
-            else:
-                with open('links.txt', 'a') as f:
-                    f.write(link['href'] + '\n')
-                    f.write('\n')
+        for link in anchors:
+            if 'href' in link.attrs:
+                if link['href'].startswith('/'):
+                    with open('links.txt', 'a') as f:
+                        f.write(url_link + link['href'] + '\n')
+                        f.write('\n')
+                else:
+                    with open('links.txt', 'a') as f:
+                        f.write(link['href'] + '\n')
+                        f.write('\n')    
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     web_url = input("Enter a web address: ")
